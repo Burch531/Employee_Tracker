6d0d7132a3f0b6ce8bc10562132ea93b58df9d70
Burchell <jlkaline@me.com>
2021-04-06 19:50:32 -0400

    add employee

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err
   console.log("Connected as Id" + connection.threadId)
-  startInquire();
+  startPrompt();
 });
 
-function startInquire() {
+function startPrompt() {
   inquirer.prompt([
     {
       type: "list",
@@ -45,6 +45,10 @@ function startInquire() {
         case "View all Emplyees By Deparments":
           viewDepartments();
         break;
+
+        case "Add Employee?":
+                addEmployee();
+              break;
 
     }
   })
@@ -76,3 +80,47 @@ function viewDepartments() {
     startPrompt()
   })
 }
+
+function addEmployee() { 
+  inquirer.prompt([
+      {
+        name: "firstname",
+        type: "input",
+        message: "Enter their first name "
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "Enter their last name "
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is their role? ",
+        choices: selectRole()
+      },
+      {
+          name: "choice",
+          type: "rawlist",
+          message: "Whats their managers name?",
+          choices: selectManager()
+      }
+  ]).then(function (val) {
+    var roleId = selectRole().indexOf(val.role) + 1
+    var managerId = selectManager().indexOf(val.choice) + 1
+    connection.query("INSERT INTO employee SET ?", 
+    {
+        first_name: val.firstName,
+        last_name: val.lastName,
+        manager_id: managerId,
+        role_id: roleId
+        
+    }, function(err){
+        if (err) throw err
+        console.table(val)
+        startPrompt()
+    })
+
+})
+}
+
