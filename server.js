7c9c8e2649033e3d0865b62e161d5cf17a44c9a6
Burchell <jlkaline@me.com>
2021-04-06 19:18:29 -0400

    update view employee

--- server.js
+++ server.js
@@ -3,34 +3,52 @@ const mysql = require("mysql")
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3000,
-    user: "root",
-    password: "JRobert202",
-    database: "employee_trackerDB"
-  });
+  host: "localhost",
+  port: 3000,
+  user: "root",
+  password: "JRobert202",
+  database: "employee_trackerDB"
+});
 
-  connection.connect(function(err) {
-    if (err) throw err
-    console.log("Connected as Id" + connection.threadId)
-    startInquire();
+connection.connect(function (err) {
+  if (err) throw err
+  console.log("Connected as Id" + connection.threadId)
+  startInquire();
 });
 
 function startInquire() {
   inquirer.prompt([
-  {
-  type: "list",
-  message: "What would you like to do?",
-  name: "choice",
-  choices: [
-            "View All Employees?", 
-            "View All Employee's By Roles?",
-            "View all Emplyees By Deparments", 
-            "Update Employee",
-            "Add Employee?",
-            "Add Role?",
-            "Add Department?"
-          ]
-  }
-])
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees?",
+        "View All Employee's By Roles?",
+        "View all Emplyees By Deparments",
+        "Update Employee",
+        "Add Employee?",
+        "Add Role?",
+        "Add Department?"
+      ]
+    }
+  ]).then(function (val) {
+    switch (val.choice) {
+      case "View All Employees?":
+        viewEmployees();
+        break;
+
+
+
+    }
+  })
+}
+
+function viewEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+    function (err, res) {
+      if (err) throw err
+      console.table(res)
+      startPrompt()
+    })
 }
